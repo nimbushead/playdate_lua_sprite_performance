c024b66811d874e20ef99f0524caf22549ea36f1
nimbushead <154494031+nimbushead@users.noreply.github.com>
2023-12-21 00:13:50 -0700

    Add files via upload

---
+++ collision-movement-collisions-main.lua
@@ -0,0 +1,23 @@
+import 'CoreLibs/object'
+import 'CoreLibs/sprites'
+
+class('MovingSprite', {
+	dx = 0,
+	dy = 0
+}).extends(playdate.graphics.sprite)
+
+function MovingSprite:update()
+	self:moveWithCollisions(self.x + self.dx, self.y + self.dy)
+end
+
+for i = 1, 40 do
+	local sprite <const> = MovingSprite()
+	sprite:setGroups({1})
+	sprite:setCollidesWithGroups({1})
+	sprite:setCollideRect(0, 0, 1, 1)
+	sprite:add()
+end
+
+playdate.display.setRefreshRate(50)
+
+playdate.update = playdate.graphics.sprite.update
---
+++ overlapping-collisions-main.lua
@@ -0,0 +1,26 @@
+import 'CoreLibs/object'
+import 'CoreLibs/sprites'
+
+class('MovingSprite', {
+	dx = 0,
+	dy = 0
+}).extends(playdate.graphics.sprite)
+
+function MovingSprite:update()
+	self:moveTo(self.x + self.dx, self.y + self.dy)
+end
+
+for i = 1, 8 do
+	local sprite <const> = MovingSprite()
+	sprite:setGroups({1})
+	sprite:setCollidesWithGroups({1})
+	sprite:setCollideRect(0, 0, 1, 1)
+	sprite:add()
+end
+
+playdate.display.setRefreshRate(50)
+
+function playdate.update()
+	playdate.graphics.sprite.update()
+	playdate.graphics.sprite.allOverlappingSprites()
+end
